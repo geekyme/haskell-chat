4484979f8ef47a79ca665175bae9841a8b80999c
Shawn Lim <shawnlim89@live.com>
2017-09-24 17:56:53 +0800

    Add encoding and decoding to the chat messages

--- backend/WebChat.hs
+++ backend/WebChat.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
+import Data.Aeson as Aeson (ToJSON, FromJSON, encode, decode)
+import GHC.Generics
 import qualified Control.Concurrent as Concurrent
 import qualified Control.Exception as Exception
 import qualified Control.Monad as Monad
@@ -15,6 +18,8 @@ import qualified Network.Wai.Handler.Warp as Warp
 import qualified Network.Wai.Handler.WebSockets as WS
 import qualified Network.WebSockets as WS
 import qualified Safe
+import Data.ByteString.Lazy (ByteString)
+import Data.ByteString.Char8 (pack)
 
 main :: IO ()
 main = do
@@ -29,14 +34,21 @@ httpApp :: Wai.Application
 httpApp _ respond =
   respond $ Wai.responseLBS Http.status400 [] "Not a websocket request"
 
+instance ToJSON ChatMsg
+instance FromJSON ChatMsg
 
 type ClientId = Int
 type Client = (ClientId, WS.Connection)
 type State = [Client]
 type Username = String
 type Message = String
-type ChatMsg = (Username, Message)
 type Chats = [ChatMsg]
+
+data ChatMsg =
+  ChatMsg
+    { username :: Username
+    , message :: Message
+    } deriving Generic
 
 nextId :: State -> ClientId
 nextId =
@@ -52,37 +64,48 @@ withoutClient :: ClientId -> State -> State
 withoutClient clientId =
   List.filter ((/=) clientId . fst)
 
-joinRoom :: ClientId -> Concurrent.MVar Chats -> IO Message
+joinRoom :: ClientId -> Concurrent.MVar Chats -> IO ByteString
 joinRoom clientId chatsRef =
   Concurrent.modifyMVar chatsRef $ \chats -> do
-    let message = show clientId ++ " has joined the room."
-    return (chats ++ [("system", message)], message)
+    let chatMsg = ChatMsg "system" $ show clientId ++ " has joined the room."
+    return (chats ++ [ chatMsg ], Aeson.encode chatMsg)
 
-leaveRoom :: ClientId -> Concurrent.MVar Chats -> IO Message
+leaveRoom :: ClientId -> Concurrent.MVar Chats -> IO ByteString
 leaveRoom clientId chatsRef =
   Concurrent.modifyMVar chatsRef $ \chats -> do
-    let message = show clientId ++ " has left the room."
-    return (chats ++ [("system", message)], message)
+    let chatMsg = ChatMsg "system" $ show clientId ++ " has left the room."
+    return (chats ++ [ chatMsg ], Aeson.encode chatMsg)
+
+newMessage :: ClientId -> ByteString -> Concurrent.MVar Chats -> IO ()
+newMessage clientId bytestring chatsRef =
+  let
+    m =  Aeson.decode bytestring :: Maybe ChatMsg
+  in
+    Concurrent.modifyMVar_ chatsRef $ \chats -> do
+      case m of
+        Nothing -> return chats
+        Just chatMsg -> return $ chats ++ [ chatMsg ]
 
 disconnectClient :: ClientId -> Concurrent.MVar State -> IO ()
 disconnectClient clientId stateRef =
   Concurrent.modifyMVar_ stateRef $ \state ->
     return $ withoutClient clientId state
 
-listen :: WS.Connection -> ClientId -> Concurrent.MVar State -> IO ()
-listen conn clientId stateRef =
+listen :: WS.Connection -> ClientId -> Concurrent.MVar State -> Concurrent.MVar Chats -> IO ()
+listen conn clientId stateRef chatsRef =
   Monad.forever $ do
-    msg <- WS.receiveData conn
-    emit stateRef msg
+    bytestring <- WS.receiveData conn
+    newMessage clientId bytestring chatsRef
+    emit stateRef bytestring
 
-broadcast :: ClientId -> Concurrent.MVar State -> Text.Text -> IO ()
+broadcast :: ClientId -> Concurrent.MVar State -> ByteString -> IO ()
 broadcast clientId stateRef msg = do
   clients <- Concurrent.readMVar stateRef
   let otherClients = withoutClient clientId clients
   Monad.forM_ otherClients $ \(_, conn) ->
     WS.sendTextData conn msg
 
-emit :: Concurrent.MVar State -> Text.Text -> IO ()
+emit :: Concurrent.MVar State -> ByteString -> IO ()
 emit stateRef msg = do
   clients <- Concurrent.readMVar stateRef
   Monad.forM_ clients $ \(_, conn) ->
@@ -92,13 +115,13 @@ wsApp :: Concurrent.MVar State -> Concurrent.MVar Chats -> WS.ServerApp
 wsApp stateRef chatsRef pendingConn = do
   conn <- WS.acceptRequest pendingConn
   clientId <- connectClient conn stateRef
-  message <- joinRoom clientId chatsRef
-  broadcast clientId stateRef $ Text.pack message
+  bytestring <- joinRoom clientId chatsRef
+  broadcast clientId stateRef bytestring
   WS.forkPingThread conn 30
   Exception.finally
-    (listen conn clientId stateRef)
+    (listen conn clientId stateRef chatsRef)
     (do
-      message <- leaveRoom clientId chatsRef
-      broadcast clientId stateRef $ Text.pack message
+      bytestring <- leaveRoom clientId chatsRef
+      broadcast clientId stateRef bytestring
       disconnectClient clientId stateRef
     )
--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -6,6 +6,9 @@ import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 import WebSocket
 import Tuple exposing (mapFirst)
+import Json.Decode as JD
+import Json.Encode as JE
+import Json.Decode.Pipeline as JDP
 
 main : Program Flags Model Msg
 main =
@@ -21,43 +24,65 @@ type alias Model =
   , chats : List ChatMsg
   , chatBoxText: Message
   , username : Username
+  , error: Maybe Error
   }
 
+type alias Error = String
 type alias Username = String
 type alias Message = String
-type alias ChatMsg = (Username, Message)
+type alias ChatMsg =
+  { username : Username
+  , message : Message
+  }
+
+decoderChatMsg : JD.Decoder ChatMsg
+decoderChatMsg =
+  JDP.decode ChatMsg
+    |> JDP.required "username" JD.string
+    |> JDP.required "message" JD.string
 
 type alias Flags =
   { username : Username
   }
 
 type Msg
-  = Receive String
-  | Send
-  | ReceiveChatMsg ChatMsg
+  = ReceiveChatMsg ChatMsg
   | TypeMsg Message
   | SendChatMsg
+  | AppError Error
 
 init : Flags -> (Model, Cmd Msg)
 init { username } =
-  (Model 0 [] "" username, Cmd.none)
+  (Model 0 [] "" username Nothing, Cmd.none)
 
 view : Model -> Html Msg
 view model =
   div []
     [ p [] [ text <| "Pokes: " ++ toString model ]
-    , button [ onClick Send ] [ text "Poke others" ]
     , div []
         (List.map viewMessage model.chats)
     , Html.form [ onSubmit SendChatMsg ]
         [ input [ type_ "text", onInput TypeMsg, value model.chatBoxText ] [] ]
+    , viewError model
     ]
 
 viewMessage : ChatMsg -> Html Msg
-viewMessage (username, message) =
+viewMessage { username, message } =
   div []
-    [ text message
+    [ text username
+    , text ": "
+    , text message
     ]
+
+viewError : Model -> Html Msg
+viewError model =
+  case model.error of
+    Just error ->
+      div []
+        [ text error ]
+
+    Nothing ->
+      text ""
 
 wsUrl : String
 wsUrl = "ws://localhost:3000"
@@ -65,12 +90,6 @@ wsUrl = "ws://localhost:3000"
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Receive "poke" ->
-      incPokes model ! []
-    Receive _ ->
-      model ! []
-    Send ->
-      model ! [ WebSocket.send wsUrl "poke" ]
     SendChatMsg ->
       model
         |> sendChatMsg
@@ -78,19 +97,32 @@ update msg model =
 
     ReceiveChatMsg chatMsg ->
       appendMsg chatMsg model ! []
+
     TypeMsg value ->
       setChatBoxText value model ! []
+
+    AppError error ->
+      setError (Just error) model ! []
 
 
 sendChatMsg : Model -> (Model, Cmd msg)
 sendChatMsg model =
   let
-    msg = model.username ++ ": " ++ model.chatBoxText
+    value =
+      JE.object
+        [ ("username", JE.string model.username)
+        , ("message", JE.string model.chatBoxText)
+        ]
+
+    msg = JE.encode 0 value
   in
     model ! [WebSocket.send wsUrl msg]
 
 setChatBoxText : Message -> Model -> Model
 setChatBoxText value model = { model | chatBoxText = value }
+
+setError : Maybe Error -> Model -> Model
+setError error model = { model | error = error }
 
 incPokes : Model -> Model
 incPokes model = { model | pokes = model.pokes + 1 }
@@ -101,7 +133,7 @@ appendMsg msg model = { model | chats = model.chats ++ [msg] }
 subscriptions : Model -> Sub Msg
 subscriptions model =
   WebSocket.listen wsUrl (\s ->
-    case s of
-      "poke" -> Receive s
-      _ -> ReceiveChatMsg ("", s)
+    case JD.decodeString decoderChatMsg s of
+      Ok chatMsg -> ReceiveChatMsg chatMsg
+      Err err -> AppError err
     )
